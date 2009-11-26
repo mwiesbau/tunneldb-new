37bf84331cbf2d93d194096b93f8bc9517bb1d8e
Mathias Wiesbauer <mathias.wiesbauer@gmail.com>
2009-11-26 11:08:34 -0500

    changed deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,17 +31,14 @@ set :user, "webadmin"
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
- namespace :deploy do
-   task :start {}
-   task :stop {}
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
-   
-    [:start, :stop].each do |t|
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+ 
+  [:start, :stop].each do |t|
     desc "#{t} task is a no-op with mod_rails"
     task t, :roles => :app do ; end
   end
-
-   
- end
+end
