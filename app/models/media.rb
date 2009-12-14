abc7a41acd96cea35bf1e7d99a7349612528c3e0
unknown <mwiesbauer@.(none)>
2009-12-14 13:15:09 -0500

    deleted files manually

--- app/models/media.rb
+++
@@ -1,9 +0,0 @@
-require 'paperclip'
-class Media < ActiveRecord::Base
-has_one			:project
-# Paperclip
-has_attached_file :photo, :styles => { :original => '800x800>',
-                               :medium => '200x200',
-                               :small => '75x75' }
-							   
-end
--- public/javascripts/create.js
+++
@@ -1,2 +0,0 @@
-page.insert_html :bottom, "pictures", :partial => 'pictures/list_item', :object => @picture
-page.visual_effect :highlight, "picture_#{@picture.id}" 
